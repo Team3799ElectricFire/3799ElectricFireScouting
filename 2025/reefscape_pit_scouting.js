340bf13df38adfea4725f570f0a75f0f1320b9cf
WildBillThe2nd <34014166+WildBillThe2nd@users.noreply.github.com>
2025-06-06 21:13:08 -0400

    Update reefscape_pit_scouting.js
    Updated to NYRO2 Flower City Frenzie

--- 2025/reefscape_pit_scouting.js
+++ 2025/reefscape_pit_scouting.js
@@ -21,7 +21,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025nyro",
+      "defaultValue": "2025nyro2",
       "required": "true"
     },
     { "name": "Width",
