d10b97f09adec3dab1f441faf1e6fb0350c6fa78
WildBillThe2nd <34014166+WildBillThe2nd@users.noreply.github.com>
2025-06-06 21:12:00 -0400

    Update reefscape_config.js
    Changed event to NYRO2 Flower City Frenzie

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025nyro",
+      "defaultValue": "2025nyro2",
       "required": "true"
     },
     { "name": "Match Level",
